feb7759a4e54f06bd369d955afd3d933780932fa
Taehyun-Koh <taehyun1018@gmail.com>
2023-02-10 13:52:00 +0900

    20230210

--- greedy/21758_g5.py
+++ greedy/21758_g5.py
@@ -2,8 +2,29 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-tmp = list(map(int, input().split(" ")))
+arr = list(map(int, input().split(" ")))
+total = sum(arr)
+answer = 0
+case = []
 
-arr = tmp[::-1]
+# case1 bee bee home
+tmp = arr[0]
+for i in range(1,n):
+    tmp += arr[i]
+    answer = max(answer, total - arr[0] + total - tmp - arr[i])
 
-print(arr)
+# case2 bee home bee
+m = max(arr[1:len(arr)])
+answer = max(answer,sum(arr[1:len(arr)-1]) + m)
+
+# case3 home bee bee
+
+arr.reverse()
+tmp = arr[0]
+for i in range(1,n):
+    tmp += arr[i]
+    answer = max(answer, total - arr[0] + total - tmp - arr[i])
+
+print(answer)
+
+## 풀이 x 2시간
